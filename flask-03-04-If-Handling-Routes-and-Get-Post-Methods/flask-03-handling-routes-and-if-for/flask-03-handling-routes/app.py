2f23cf35d1ef1ab7f130d48816f8e5ec9a2edb08
zeliha <aynizeliha.firat@gmail.com>
2023-10-23 20:23:05 -0400

    add kittens carousel files

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask-03-handling-routes/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask-03-handling-routes/app.py
@@ -1,22 +1,28 @@
 #Import Flask modules
-
+from flask import Flask,render_template
 
 #Create an object named app 
+app = Flask(__name__)
 
 
 # Create a function named home which returns a string 'This is home page for no path, <h1> Welcome Home</h1>' 
 # and assign route of no path ('/')
-
+@app.route("/")
+def home():
+    return'This is home page for no path, <h1> Welcome Home</h1>' 
 
 
 # Create a function named about which returns a formatted string '<h1>This is my about page </h1>' 
 # and assign to the static route of ('about')
+@app.route("/about")
+def about():
+    return '<h1>This is my about page </h1>'
 
 
 # Create a function named error which returns a formatted string '<h1>Either you encountered an error or you are not authorized.</h1>' 
 # and assign to the static route of ('error')
 
-
+@app.route("/error")
 
 # Create a function named admin which redirect the request to the error path 
 # and assign to the route of ('/admin')
@@ -53,3 +59,5 @@
 
 
 # Add a statement to run the Flask application which can be reached from any host.
+if __name__=='__main__':
+    app.run("0.0.0.0",port=5001)
