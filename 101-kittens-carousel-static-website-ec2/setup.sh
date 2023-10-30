620ed6dcdfab5c36825a0d7dcbfaa5828be47578
zeliha <aynizeliha.firat@gmail.com>
2023-10-30 19:25:20 -0400

    adding roman numerals files

--- 101-kittens-carousel-static-website-ec2/setup.sh
+++ 101-kittens-carousel-static-website-ec2/setup.sh
@@ -1,11 +1,17 @@
+#!/bin/bash -x
+
+# update the system and install apache
 yum update -y
 yum install httpd -y
+
+# change to the correct directory and download web content
 cd /var/www/html
-FOLDER=https://raw.githubusercontent.com/zeliha21/my-repositoryy/main/101-kittens-carousel-static-website-ec2/static-web
+FOLDER=https://raw.gicontent.com/zeliha21/my-repositoryy/main/101-kittens-carousel-static-website-ec2/static-webthubuser
 wget ${FOLDER}/index.html
 wget ${FOLDER}/cat0.jpg
 wget ${FOLDER}/cat1.jpg
 wget ${FOLDER}/cat2.jpg
 
+# start and enable httpd
 systemctl start httpd
 systemctl enable httpd
